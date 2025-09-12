453a96c762b007f169efca20146ae35208f15bb3
MinotaurG <adixavi7@live.com>
2025-09-12 21:31:33 +0530

    feat(day3): add environment setup with pom.xml, Main, Book, User

---
+++ src/main/java/com/bookstore/Book.java
@@ -0,0 +1,24 @@
+package com.bookstore;
+
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    // Constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // getters
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public double getPrice() { return price; }
+
+    @Override
+    public String toString() {
+        return title + " by " + author + " ($" + price + ")";
+    }
+}
